efc1702d79b4215c76a4924533220c58886cb433
Jeremy Pry <jeremy.pry@gmail.com>
2013-04-03 14:46:38 -0300

    Call the WP_Object_Cache::flush method

--- add-wp-cache-reset-function.php
+++ add-wp-cache-reset-function.php
@@ -3,7 +3,7 @@
  * Plugin Name: Add wp_cache_reset() function
  * Plugin URI: https://github.com/JCPry/WPE-add-wp_cache_reset-function
  * Description: For compatibility, ensure that the wp_cache_reset() function exists.
- * Version: 1.0
+ * Version: 1.1
  * Author: Jeremy Pry
  * Author URI: http://jeremypry.com/
  * License: GPL3
@@ -13,7 +13,9 @@ if ( ! function_exists( 'wp_cache_reset' ) ) {
     function wp_cache_reset() {
         _deprecated_function( __FUNCTION__, '3.5' );
         
-        // Doesn't do anything else. Just return true
-        return true;
+        global $wp_object_cache;
+        
+        // While we should use the WP_Object_Cache::reset() method, that might not exist
+        return $wp_object_cache->flush();
     }
 }
